e871c9a3f66c22b392f6a674c5e6408b8beac742
kchzhao <kamila.chzhao@gmail.com>
2022-10-28 06:29:09 -0400

    Initial commit

---
+++ src/Main.java
@@ -0,0 +1,30 @@
+public class Main {
+    public static void main(String[] args) {
+        //HW 1 Exercise 1
+        System.out.println("HW 1 Exercise 1");
+        String firstName = "Ivan ";
+        String middleName = "Ivanovich";
+        String lastName = "Ivanov ";
+        String fullName = lastName + firstName + middleName;
+        System.out.println("ФИО сотрудника — " + fullName + ".");
+
+        //HW 1 Exercise 2
+        System.out.println();
+        System.out.println("HW 1 Exercise 2");
+        fullName = fullName.toUpperCase();
+        System.out.println("Данные ФИО сотрудника для заполнения отчета — " + fullName + ".");
+
+        //HW 1 Exercise 3
+        System.out.println();
+        System.out.println("HW 1 Exercise 3");
+        firstName = "Семён ";
+        middleName = "Семёнович";
+        lastName = "Иванов ";
+        fullName = lastName + firstName + middleName;
+        if (fullName.contains("ё")) {
+            fullName = fullName.replace("ё","е");
+        }
+        System.out.println("Данные ФИО сотрудника — " + fullName + ".");
+
+    }
+}
